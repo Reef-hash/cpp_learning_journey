c807a85c89214f344a21064a8c836f267d11752f
Reef-hash <riffsyaz@gmail.com>
2026-03-28 01:31:33 +0800

    writing constant variable

--- 01_Basics/03_input_output.cpp
+++ 01_Basics/03_input_output.cpp
@@ -5,7 +5,7 @@ int main() {
     // Declare a variable to store the user's name
     std::string name; //only declare the variable without initializing it
 
-    std::cout << "Please enter your name: "std::endl; // Prompt the user to enter their name
+    std::cout << "Please enter your name: " << std::endl; // Prompt the user to enter their name
 
     std::getline(std::cin, name); // Read the user's input and store it in the 'name' variable
     //using getline(cin, name) allows us to read entire line of input instead of just a single word.
---
+++ 01_Basics/04_constant.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+
+    const double PI = 3.14159; // Declare a constant variable for the value of pi
+    std::cout << "The value of pi is: " << PI << std::endl; // Output the value of pi
+    
+    // PI = 3.14; // This line would cause a compilation error because PI is a constant and cannot be modified
+
+
+    return 0;
+}
