2737835ecdf72a7f669443ec7616fc2fa693c6a2
Reef-hash <riffsyaz@gmail.com>
2026-03-27 17:37:26 +0800

    Day 1: Hello,world!

---
+++ 01_Basics/hello_world.cpp
@@ -0,0 +1,12 @@
+#include <iostream>     // Include the iostream library for input/output operations
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;           // Return 0 to indicate that the program ended successfully
+
+}
+
+//This is comment
+
+/* This is a multi-line comment
+   that can span multiple lines. */
