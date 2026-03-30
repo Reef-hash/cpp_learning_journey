91fd750102ca8e2deea4d514c757b29fd609b97d
Reef-hash <riffsyaz@gmail.com>
2026-03-30 19:06:29 +0800

    <feat>learn how to use and print arithmatic operation

---
+++ 02_operators_expressions/arithmatic.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+
+int main()  {
+
+    int x = 10;
+    int y = 15;
+
+    std::cout << "====ARITHMATIC OPERATION====" << std::endl;
+
+    std::cout << "\nx + y: " << x + y << std::endl; //this is addition operator
+    std::cout << "x - y: " << x - y << std::endl; //this is subtraction operator
+    std::cout << "x * y: " << x * y << std::endl; //this is multiplication operator
+    std::cout << "x / y: " << x / y << std::endl; //this is division operator
+    std::cout << "x % y: " << x % y << std::endl; //this is modulus operator
+
+    
+    return 0;
+}
