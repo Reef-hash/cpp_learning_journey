52edb1f9c713e2cd020a49ce68148609738a6290
Reef-hash <riffsyaz@gmail.com>
2026-03-30 19:17:37 +0800

    <feat>ask for user input and print * we can literally change value of variables coz its not const * i'll add more..

--- 02_operators_expressions/arithmatic.cpp
+++ 02_operators_expressions/arithmatic.cpp
@@ -9,11 +9,21 @@ int main()  {
     std::cout << "====ARITHMATIC OPERATION====" << std::endl;
 
     std::cout << "\nx + y: " << x + y << std::endl; //this is addition operator
-    std::cout << "x - y: " << x - y << std::endl; //this is subtraction operator
-    std::cout << "x * y: " << x * y << std::endl; //this is multiplication operator
-    std::cout << "x / y: " << x / y << std::endl; //this is division operator
-    std::cout << "x % y: " << x % y << std::endl; //this is modulus operator
+    std::cout << "x - y: " << x - y << std::endl;   //this is subtraction operator
+    std::cout << "x * y: " << x * y << std::endl;   //this is multiplication operator
+    std::cout << "x / y: " << x / y << std::endl;   //this is division operator
+    std::cout << "x % y: " << x % y << std::endl;   //this is modulus operator
 
-    
+    //..ask input and print for user with each operation
+    std::cout << "\ninsert value of x: "; std::cin >> x;
+    std::cout << "insert value of y: "; std::cin >> y;
+
+    //..printing..
+    std::cout << "x + y: " << x + y << std::endl;  //we can change x and y value because it is not constant variables..
+    std::cout << "x - y: " << x - y << std::endl; 
+    std::cout << "x * y: " << x * y << std::endl; 
+    std::cout << "x / y: " << x / y << std::endl; 
+    std::cout << "x % y: " << x % y << std::endl; 
+
     return 0;
 }
