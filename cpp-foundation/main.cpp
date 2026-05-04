f644b0c89102cb917d5280233807f53c6f1e56cd
Reef-hash <riffsyaz@gmail.com>
2026-05-05 03:48:27 +0800

    just added a comment

--- cpp-foundation/main.cpp
+++ cpp-foundation/main.cpp
@@ -4,7 +4,18 @@ using namespace std; //this is not good for big project
 
 int main() {
 
-    cout << "Hello, World!" << " Welcome to my learning journey..." << endl;
+    cout << "Hello, World!" << " Welcome to C++ foundation" << endl;
 
     return 0;
 }
+
+//This is comment
+
+?
+/*
+This
+is
+a 
+multi-line
+comment
+*/
